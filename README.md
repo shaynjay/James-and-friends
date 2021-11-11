bf93e4348a8fa6faae2d4556fb233412d2566431
shaynjay <shaysavidge1@gmail.com>
2021-11-11 10:29:33 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-vyy8ue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-vyy8ue)
---
+++ script.js
@@ -0,0 +1,5 @@
+"""console.log('Weloome to life, my name is James Nicholas Savidge and I am your truth and savour, please continue on!')
+console.log(...AbortSignal: Jack Hilton)
+console.log(...status: Finished[])
+console.timeLog{'DragonBall Z'("Goku & Vegeta  & Bulma & ChiChi live forever in all time and space")
+"""
